0cd1a061bb76e112f39da44d44c7525021f1456d
sivasathyaseeelan <dnsiva.sathyseelan.chy21@iitbhu.ac.in>
2024-04-01 22:03:31 +0530

    project done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
-use std::net::{IpAddr};
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::process;
 use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
+const MAX: u16 = 65535;
 
 
 struct Arguments {
@@ -56,6 +60,25 @@ impl Arguments {
 }
 
 
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX -port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -68,4 +91,27 @@ fn main() {
             process::exit(0);
         }
     });
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx,rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
